4af51be7d2374be4fdee21c598c945d5e8b32e53
leehyeonseok <leehyeonseok@localhost>
2022-01-11 00:23:30 +0900

    test

--- src/main/java/com/myspring/pro30/board/controller/BoardControllerImpl.java
+++ src/main/java/com/myspring/pro30/board/controller/BoardControllerImpl.java
@@ -118,7 +118,7 @@ public class BoardControllerImpl implements BoardController {
 					}
 				}
 				mFile.transferTo(new File(ARTICLE_IMAGE_REPO+"/"+imageFileName));
-			} 
+			}  
 		}
 		return imageFileName;
 	}
