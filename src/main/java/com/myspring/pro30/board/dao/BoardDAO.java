5e06440dba4b8668e983a7b06808b5cbd2a63894
np943 <dlgustjr1024@naver.com>
2022-01-12 15:24:40 +0900

    30장 끝

--- src/main/java/com/myspring/pro30/board/dao/BoardDAO.java
+++ src/main/java/com/myspring/pro30/board/dao/BoardDAO.java
@@ -10,8 +10,15 @@ import com.myspring.pro30.board.vo.ArticleVO;
 public interface BoardDAO {
 	public List selectAllArticlesList() throws Exception;
 	public int insertNewArticle(Map articleMap) throws DataAccessException;
+	public void insertNewImage(Map articleMap);
+
 	public ArticleVO selectArticle(int articleNO) throws DataAccessException;
 	public void updateArticle(Map articleMap) throws DataAccessException;
 	public void deleteArticle(int articleNO) throws DataAccessException;
-	//public List selectImageFileList(int articleNO) throws DataAccessException;
+	
+	//다중파일
+	public List selectImageFileList(int articleNO) throws DataAccessException;
+	
+	//public int insertNewImage(Map articleMap);
+	
 }
